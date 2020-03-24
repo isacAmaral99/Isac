6a24d819af0281bd17dda15f59fa06178fbfe3ef
isacAmaral99 <isacamaralbueno@gmail.com>
2020-03-24 12:20:04 -0300

    teste de tag

---
+++ ContaCorrente.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoUnip
+{
+    public class ContaCorrente
+    {
+        private bool IsNullOrEmpty;
+
+        // precisa ter 
+        public string Nome { get; set; }
+        public int ContaNumero { get; set; }
+        public string AgenciaNumero { get; set; }
+        private double Saldo { get; set; }
+
+
+        public double saldo
+        {
+            get
+            {
+                return Saldo;
+            }
+            set 
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                Saldo = value;
+            }
+        }
+        
+        //public ContaCorrente(string nome,int contaNumero, string agenciaNumero)
+        //{
+        //    this.Nome = nome;
+        //    this.AgenciaNumero = agenciaNumero;
+        //    this.ContaNumero = contaNumero;
+            
+        //}
+        public bool Sacar(double valor)
+        {
+            if (this.Saldo < valor)
+            {
+                
+                return false;
+                
+            }
+            else
+            {
+                this.Saldo -= valor;
+                return true;
+            }
+            
+        }
+        
+        public void Depositar(double valor)
+        {
+            this.Saldo += valor;
+        }
+         public bool VerificaArgumentos(ContaCorrente contaCorrente)
+        {
+            if (Nome=="" || ContaNumero.ToString()=="" || AgenciaNumero.ToString()=="")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoUnip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Modificando
+            ContaCorrente contaCorrente = new ContaCorrente();
+            Console.WriteLine("Escreva o nome do titular:");
+            contaCorrente.Nome = Convert.ToString(Console.ReadLine());
+            
+            Console.WriteLine("informe sua agencia:");
+            contaCorrente.AgenciaNumero = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("informe o numero de sua conta:");
+            contaCorrente.ContaNumero = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Coloque seu saldo atual:");
+            contaCorrente.saldo = Convert.ToDouble(Console.ReadLine());
+
+
+            contaCorrente.VerificaArgumentos(contaCorrente);
+            
+            if(contaCorrente.VerificaArgumentos(contaCorrente)==false)
+            {
+                Console.WriteLine("Existem campos Em branco por favor preencha novamente");
+            }
+
+            ExibeInformacoes(contaCorrente);
+            contaCorrente.Depositar(1500);
+            Console.WriteLine($"Apos o deposito seu saldo é de : R${contaCorrente.saldo}");
+            contaCorrente.Sacar(750);
+            Console.WriteLine($"apos o saque, seu saldo atual é de :R${contaCorrente.saldo}.");
+            Console.ReadLine();
+
+        }
+
+        private static void ExibeInformacoes(ContaCorrente contaCorrente)
+        {
+            Console.WriteLine($"Exibindo informaçoes sobre a conta de;{contaCorrente.Nome}");
+            Console.WriteLine($"O numero da sua Agencia é:{contaCorrente.AgenciaNumero}");
+            Console.WriteLine($"o numero da sua conta é:{contaCorrente.ContaNumero}");
+            Console.WriteLine($"o seu saldo atual é de:{contaCorrente.saldo}");
+            return;
+        }
+    }
+}
